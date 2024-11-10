b7ccf83dcae16c0b28258888df94a2c7febffe33
teacher <tibedox@mail.ru>
2024-11-10 14:36:29 +0600

    array of sheeps

--- core/src/main/java/ru/sunday/sunsheep/Main.java
+++ core/src/main/java/ru/sunday/sunsheep/Main.java
@@ -11,24 +11,28 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
-    Sheep sheep0, sheep1;
+    Sheep[] sheep = new Sheep[3];
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("sheep0.png");
-        sheep0 = new Sheep(10, 50, 100, 100);
-        sheep1 = new Sheep(400, 300, 80, 80);
+        sheep[0] = new Sheep(10, 50, 100, 100);
+        sheep[1] = new Sheep(400, 300, 80, 80);
+        sheep[2] = new Sheep(800, 300, 150, 150);
     }
 
     @Override
     public void render() {
-        sheep0.fly();
-        sheep1.fly();
+        for (int i = 0; i < sheep.length; i++) {
+            sheep[i].fly();
+        }
+
         ScreenUtils.clear(0.45f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, sheep0.x, sheep0.y, sheep0.width, sheep0.height);
-        batch.draw(image, sheep1.x, sheep1.y, sheep1.width, sheep1.height);
+        for (int i = 0; i < sheep.length; i++) {
+            batch.draw(image, sheep[i].x, sheep[i].y, sheep[i].width, sheep[i].height);
+        }
         batch.end();
     }
 
